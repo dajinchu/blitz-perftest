dde5ee6e91f853720e54a08c1f1d57eb41d74780
dajinchu <dajinchu@gmail.com>
2020-05-12 23:38:47 -0400

    log

--- app/api/test.ts
+++ app/api/test.ts
@@ -1,11 +1,12 @@
 import { NextApiResponse, NextApiRequest } from "next";
-import db  from 'db'
+import db from 'db'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const products = await db.product.findMany({ first: 4 })
-    return products
-  } catch {
+    res.status(200).json(products)
+  } catch (e) {
+    console.error(e)
     res.status(500).send('')
   }
 }
--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from "@prisma/client"
 export * from "@prisma/client"
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+  log: ['info']
+})
 
 export default prisma
